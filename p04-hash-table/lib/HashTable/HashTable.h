7092987a3cbca84141b4de4019b635e3f2a23254
Estela_fdz <alu0101560205@ull.edu.es>
2024-03-13 12:24:21 +0000

    HashTable

---
+++ p04-hash-table/lib/HashTable/HashTable.h
@@ -0,0 +1,35 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Algoritmos y Estructuras de Datos Avanzada
+  *
+  * @file HashTable.h
+  * @author Estela Fernández Trujillo alu0101560205@ull.edu.es
+  * @date Mar 13 2024
+  */
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include "../Sequence/Sequence.h"
+#include "../Sequence/StaticSequence.h"
+#include "../Sequence/DynamicSequence.h"
+#include "../Dispersion/DispersionFunction.h"
+#include "../Exploration/ExplorationFunction.h"
+
+template <class Key, class Container = StaticSequence<Key>>
+class HashTable : public Sequence<Key> {
+  public:
+    HashTable<Key,Container>::HashTable(unsigned sz, DispersionFunction<Key>& fd, ExplorationFunction<Key>& fe,unsigned bsz)
+      : tableSize_(sz), fd_(fd), fe_(fe), blockSize_(bsz) {
+        table_ = new Container[tableSize_];
+      }
+  private:
+    size_t tableSize_; 
+    Container* table_;
+    DispersionFunction<key>& fd_;
+    ExplorationFunction<Key>& fe_;
+    size_t blockSize_;
+};
+
+#endif
