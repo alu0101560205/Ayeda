c69e000b05a819a9d7c66e4f7349a39af7aabe1c
Estela_fdz <alu0101560205@ull.edu.es>
2024-04-21 17:24:24 +0100

    Arbol de búsqueda implementado

--- p06-arboles/lib/ABB/ABB.h
+++ p06-arboles/lib/ABB/ABB.h
@@ -9,3 +9,48 @@
   * @date April 20 2024
   */
 
+#ifndef ABB_H
+#define ABB_H
+#include <iostream>
+#include "../AB/AB.h"
+
+/**
+ * @brief Clase para representar el árbol binario de búsqueda
+*/
+template <class Key>
+class ABB : public AB<Key> {
+  public:
+    ABB() : AB<Key>() {}
+    bool insertar(const Key& k) override { InsertarRama(this->raiz_, k); }
+    bool buscar(const Key& k) const override { BuscarRama(this->raiz_, k); }
+  private:
+    NodoB<Key>* BuscarRama(NodoB<Key>* nodo, Key clave) const;
+    void InsertarRama(NodoB<Key>* &nodo, Key clave);
+};
+
+/**
+ * @brief Método auxiliar para realizar la búsqueda
+*/
+template <class Key>
+NodoB<Key>* ABB<Key>::BuscarRama(NodoB<Key>* nodo, Key clave) const {
+  if (nodo == NULL) return NULL;
+  if (clave == nodo->getDato()) return nodo;
+  if (clave < nodo->getDato()) return BuscarRama(nodo->getIzq(), clave);
+  return BuscarRama(nodo->getDer(), clave);
+}
+
+/**
+ * @brief Método auxiliar para la insercción en árbol de búsqueda
+*/
+template <class Key>
+void ABB<Key>::InsertarRama(NodoB<Key>* &nodo, Key clave) {
+  if (nodo == NULL) {
+    nodo = new NodoB<Key>(clave, NULL, NULL);
+  } else if (clave < nodo->getDato()) {
+    InsertarRama(nodo->getIzq(), clave);
+  } else {
+    InsertarRama(nodo->getDer(), clave);
+  }
+}
+
+#endif
--- p06-arboles/src/main.cc
+++ p06-arboles/src/main.cc
@@ -11,11 +11,13 @@
 
 #include "../lib/AB/AB.h"
 #include "../lib/ABE/ABE.h"
+#include "../lib/ABB/ABB.h"
 #include "../lib/NodoB/NodoB.h"
 
 int main() {
 
     ABE<int> arbol;
+    ABB<int> arbolbusqueda;
 
     // Inserta algunos elementos en el árbol
     arbol.insertar(10);
@@ -25,7 +27,8 @@ int main() {
     arbol.insertar(7);
     arbol.insertar(12);
     arbol.insertar(18);
-
+    
+    std::cout << "Árbol binario equilibrado\n";
     std::cout << arbol;
     std::cout << std::endl;
 
@@ -33,14 +36,21 @@ int main() {
     arbol.inorden();
     std::cout << std::endl;
 
-    std::cout << "Búsqueda del elemento 7 en el árbol: ";
-    std::cout << arbol.buscar(8) << std::endl;
+    std::cout << "Búsqueda del elemento 8 en el árbol: ";
+    std::cout << arbol.buscar(8) << std::endl << std::endl;
 
-    // arbol.insertar(26);
-    // arbol.insertar(87);
-    // std::cout << arbol;
-    // std::cout << "Recorrido inorden:\n";
-    // arbol.inorden();
-    // std::cout << std::endl;
+    std::cout << "Arbol binario de búsqueda\n";
+    arbolbusqueda.insertar(10);
+    arbolbusqueda.insertar(8);
+    arbolbusqueda.insertar(2);
+    arbolbusqueda.insertar(15);
+    arbolbusqueda.insertar(14);
+    arbolbusqueda.insertar(3);
+    arbolbusqueda.insertar(12);
+    
+    std::cout << arbolbusqueda << std::endl;
+    std::cout << "Búsqueda del elemento 12 en el árbol: ";
+    std::cout << arbol.buscar(12) << std::endl;
+
     return 0;
 }
