149829b5a8ce5350bc2f63ed25e252c464891967
Estela_fdz <alu0101560205@ull.edu.es>
2024-04-29 17:50:40 +0100

    Avances

--- p07-arboles-AVL/lib/AVL/AVL.h
+++ p07-arboles-AVL/lib/AVL/AVL.h
@@ -21,137 +21,131 @@ template <class Key>
 class AVL : public ABB<Key> {
   public:
     AVL() : ABB<Key>(), traza_(false) {}
-    bool insertar(const Key& k) override { return InsertarRama(this->raiz_, k); }
+    bool insertar(const Key& k) override;
     void activarTraza() { traza_ = true; }
     void desactivarTraza() { traza_ = false; }
   private:
     NodoAVL<Key>* raiz_; // Raíz del árbol AVL
     bool traza_; // Atributo para indicar si el modo traza está activado
-    bool InsertarRama(NodoB<Key>*& nodo, Key clave);
-    void Equilibrar(NodoAVL<Key>*& nodo);
-    void rotacionIzquierda(NodoAVL<Key>*& nodo);
-    void rotacionDerecha(NodoAVL<Key>*& nodo);
-    int calcularBalance(NodoAVL<Key>* nodo);
+    void Rotacion_II(NodoAVL<Key>* &nodo);
+    void Rotacion_DD(NodoAVL<Key>* &nodo);
+    void Rotacion_ID(NodoAVL<Key>* &nodo);
+    void Rotacion_DI(NodoAVL<Key>* &nodo);
+    void Inserta_bal(NodoAVL<Key>* &nodo, NodoAVL<Key>* nuevo, bool& crece);
 };
 
 /**
- * @brief Método para insertar una rama
+ * @brief Método para implementar la rotación Izquierda-Izquierda
 */
 template <class Key>
-bool AVL<Key>::InsertarRama(NodoB<Key>*& nodo, Key clave) {
-  if (!nodo) {
-    nodo = new NodoAVL<Key>(clave);
-    return true;
+void AVL<Key>::Rotacion_II(NodoAVL<Key>* &nodo) {
+  NodoAVL<Key>* nodo1 = nodo->getIzq();
+  nodo->setIzq(nodo1->getDer());
+  nodo1->setDer(nodo);
+  if (nodo1->getBalance() == 1) {
+    nodo->setBalance(0);
+    nodo1->setBalance(0);
+  } else { // balance == 0
+    nodo->setBalance(1);
+    nodo1->setBalance(-1);
   }
-  if (clave < nodo->getDato()) {
-    if (!InsertarRama(nodo->getIzq(), clave)) {
-      return false;
-    }
-  } else if (clave > nodo->getDato()) {
-    if (!InsertarRama(nodo->getDer(), clave)) {
-      return false;
-    }
+  nodo = nodo1;
+}
+
+/**
+ * @brief Método para implementar la rotación Derecha-Derecha
+*/
+template <class Key>
+void AVL<Key>::Rotacion_DD(NodoAVL<Key>* &nodo) {
+  NodoAVL<Key>* nodo1 = nodo->getDer();
+  nodo->setDer(nodo1->getIzq());
+  nodo1->setIzq(nodo);
+  if (nodo1->getBalance() == -1) {
+    nodo->setBalance(0);
+    nodo1->setBalance(0);
   } else {
-    // El valor ya existe en el árbol
-    return false;
+    nodo->setBalance(-1);
+    nodo1->setBalance(1);
   }
-  // Equilibrar el nodo después de la inserción
-  NodoAVL<Key>* nodoAVL = static_cast<NodoAVL<Key>*>(nodo);
-  equilibrar(nodoAVL);
-  return true;
+  nodo = nodo1;
 }
 
 /**
- * @brief Método para equilibrar el árbol AVL después de la insercción
+ * @brief Método para implementar la rotación Izquierda-Derecha
 */
 template <class Key>
-void AVL<Key>::Equilibrar(NodoAVL<Key>*& nodo) {
-  // Calcula el factor de balanceo
-  int balance = calcularBalance(nodo);
-  // Rotación necesaria si el factor de balanceo no está dentro del rango de [-1, 1]
-  if (balance > 1) {
-    // Subárbol izquierdo está desbalanceado
-    if (calcularBalance(nodo->getIzq()) < 0) {
-      // Rotación ID (Izquierda-Derecha)
-      if (traza_) {
-        std::cout << "Árbol antes de la rotación ID en nodo " << nodo->getDato() << ":\n";
-        imprimirArbol(nodo);
-        std::cout << "Aplicando rotación ID...\n";
-      }
-      rotacionIzquierda(nodo->getIzq()); // Rotación izquierda en el subárbol izquierdo
-    }
-    // Rotación II (Izquierda-Izquierda)
-    if (traza_) {
-      std::cout << "Árbol antes de la rotación II en nodo " << nodo->getDato() << ":\n";
-      imprimirArbol(nodo);
-      std::cout << "Aplicando rotación II...\n";
-    }
-    rotacionDerecha(nodo); // Rotación derecha en el nodo actual
-  } else if (balance < -1) {
-    // Subárbol derecho está desbalanceado
-    if (calcularBalance(nodo->getDer()) > 0) {
-      // Rotación DI (Derecha-Izquierda)
-      if (traza_) {
-        std::cout << "Árbol antes de la rotación DI en nodo " << nodo->getDato() << ":\n";
-        imprimirArbol(nodo);
-        std::cout << "Aplicando rotación DI...\n";
-      }
-      rotacionDerecha(nodo->getDer()); // Rotación derecha en el subárbol derecho
-    }
-    // Rotación DD (Derecha-Derecha)
-    if (traza_) {
-      std::cout << "Árbol antes de la rotación DD en nodo " << nodo->getDato() << ":\n";
-      imprimirArbol(nodo);
-      std::cout << "Aplicando rotación DD...\n";
-    }
-    rotacionIzquierda(nodo); // Rotación izquierda en el nodo actual
+void AVL<Key>::Rotacion_ID(NodoAVL<Key>* &nodo) {
+  NodoAVL<Key>* nodo1 = nodo->getIzq();
+  NodoAVL<Key>* nodo2 = nodo1->getDer();
+  nodo->setIzq(nodo2->getDer());
+  nodo2->setDer(nodo);
+  nodo1->setDer(nodo2->getIzq());
+  nodo2->setIzq(nodo1);
+  if (nodo2->getBalance() == -1) {
+    nodo1->setBalance(1);
+  } else {
+    nodo1->setBalance(0);
+  }
+  if (nodo2->getBalance() == 1) {
+    nodo->setBalance(-1);
+  } else {
+    nodo->setBalance(0);
+  }
+  nodo2->setBalance(0);
+  nodo = nodo2;
+}
+
+/**
+ * @brief Método para implementar la rotación Derecha-Izquierda
+*/
+template <class Key>
+void AVL<Key>::Rotacion_DI(NodoAVL<Key>* &nodo) {
+  NodoAVL<Key>* nodo1 = nodo->getDer();
+  NodoAVL<Key>* nodo2 = nodo1->getIzq();
+  nodo->setDer(nodo2->getIzq());
+  nodo2->setIzq(nodo);
+  nodo1->setIzq(nodo2->getDer());
+  nodo2->setDer(nodo1);
+  if (nodo2->getBalance() == 1) {
+    nodo1->setBalance(-1);
+  } else {
+    nodo1->setBalance(0);
+  }
+  if (nodo2->getBalance() == -1) {
+    nodo->setBalance(1);
+  } else {
+    nodo->setBalance(0);
+  }
+  nodo2->setBalance(0);
+  nodo = nodo2;
+}
+
+/**
+ * @brief Método para insertar valores en el árbol
+*/
+template <class Key>
+bool AVL<Key>::insertar(const Key& k) {
+  NodoAVL<Key>* nuevo = NodoAVL<Key>*(k, nullptr, nullptr, 0);
+  bool crece = false;
+  Inserta_bal(this->raiz_, nuevo, crece);
+}
+
+/**
+ * @brief Método auxiliar de insertar
+*/
+template <class Key>
+void AVL<Key>::Inserta_bal(NodoAVL<Key>* &nodo, NodoAVL<Key>* nuevo, bool& crece) {
+  if (nodo == nullptr) {
+    nodo = nuevo;
+    crece = true;
+  } else if (nuevo->getDato() < nodo->getDato()) {
+    Inserta_bal(nodo->getIzq(), nuevo, crece);
+    if (crece) Insert_re_balancea_izq(nodo);
+  } else {
+    Inserta_bal(nodo->getDer(), nuevo, crece);
+    if (crece) Insert_re_balancea_der(nodo);
   }
 }
-
-/**
- * @brief Método para la rotación a la izquierda
-*/
-template <class Key>
-void AVL<Key>::rotacionIzquierda(NodoAVL<Key>*& nodo) {
-  NodoAVL<Key>* hijoDerecho = static_cast<NodoAVL<Key>*>(nodo->getDer());
-  nodo->setDer(hijoDerecho->getIzq());
-  hijoDerecho->setIzq(nodo);
-  nodo = hijoDerecho;
-
-  // Actualizar los factores de balanceo
-  static_cast<NodoAVL<Key>*>(nodo->getIzq())->actualizarFactorBalanceo();
-  hijoDerecho->actualizarFactorBalanceo();
-}
-
-/**
- * @brief Método para rotar a la derecha
-*/
-template <class Key>
-void AVL<Key>::rotacionDerecha(NodoAVL<Key>*& nodo) {
-  NodoAVL<Key>* hijoIzquierdo = static_cast<NodoAVL<Key>*>(nodo->getIzq());
-  nodo->setIzq(hijoIzquierdo->getDer());
-  hijoIzquierdo->setDer(nodo);
-  nodo = hijoIzquierdo;
-
-  // Actualizar los factores de balanceo
-  static_cast<NodoAVL<Key>*>(nodo->getDer())->actualizarFactorBalanceo();
-  hijoIzquierdo->actualizarFactorBalanceo();
-}
-
-/**
- * @brief Método para calcular el balanceo
-*/
-template <class Key>
-int AVL<Key>::calcularBalance(NodoAVL<Key>* nodo) {
-  if (!nodo) {
-    return 0; // Si el nodo es nulo, el balance es cero
-  }
-    
-  int alturaIzquierda = altura(static_cast<NodoAVL<Key>*>(nodo->getIzq()));
-  int alturaDerecha = altura(static_cast<NodoAVL<Key>*>(nodo->getDer()));
-
-  // Retorna la diferencia entre la altura del subárbol izquierdo y derecho
-  return alturaIzquierda - alturaDerecha;
-}
+// diapositiva 24
 
 #endif
